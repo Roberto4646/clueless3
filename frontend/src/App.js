85dc2b7bf06fa941e8913644cef39b6a848420a7
briantran987 <briantran3504@gmail.com>
2024-03-16 00:19:06 -0400

    merge via upload
    bare ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+import React from 'react';
+import { useState, useEffect} from 'react';
 
 function App() {
+  const [socket, setSocket] = useState(null);
+  const [pid, setPID] = useState(-1);
+  const [gid, setGID] = useState(-1);
+  
+  useEffect(() => {
+    const newSocket = io('http://localhost:5000');
+    setSocket(newSocket);
+  }, []);
+
+  const createLobby = () => {
+    socket.emit('LOBBY_CREATE');
+    socket.on('message', function (data) {
+      console.log(data);
+      setPID(data[0]);
+      setGID(data[1]);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        Clue-less
       </header>
+      
+        {/* buttons for each message */}
+        <button onClick={() => createLobby()}>Create Lobby</button>
+        <div>
+          The PID: {pid}
+        </div>
+        <div>The GID: {gid}</div>
+      
     </div>
   );
 }
